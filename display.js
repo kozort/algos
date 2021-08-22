c697b697946fbf92380a29305a9d2339de254f95
kozort <timkozorezov@gmail.com>
2021-08-22 16:01:29 -0700

    solution fo rdisplay()

---
+++ display.js
@@ -0,0 +1,47 @@
+class Node {
+    constructor(valu) {
+        this.value = valu;
+        this.next = null;
+    }
+}
+
+class SLL {
+    constructor(){
+        this.head = null;
+    }
+    display() {
+        str = "List of Values = "
+        while (this.head != null){
+            str += this.head.value;
+            this.head = this.next;
+        }
+        return str
+    }
+
+    addFront(value) {
+        newNode = new Node(value);
+        newNode.next = this.head;
+        this.head = newNode;
+        return this
+
+    }
+    removeFront() {
+        if (this.head == null){
+            return null
+        }
+        this.head = this.next;
+        this.next = null
+        return this
+    }
+    front() {
+        if (this.head == null){
+            return null
+        }
+        return this.head.value
+    }
+}
+
+
+
+
+
